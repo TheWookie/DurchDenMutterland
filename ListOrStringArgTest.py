4942fc678196e6913947acf60598d0f1190109a2
Son of Lysander <paul.n.baker@utah.edu>
2014-05-25 16:32:26 -0600

    Fixing typos.

--- ListOrStringArgTest.py
+++ ListOrStringArgTest.py
@@ -6,22 +6,22 @@ def test_one(*word_or_words):
     print()
         
 def test_two(*wordz):
-    if "penis" in wordz:
-        print("penis in wordz.")
+    if "pens" in wordz:
+        print("pens in wordz.")
     else:
-        print("no d here!")
+        print("no pen here!")
     print()
     
 def test_three(*words):
     print(words)
     words = list(words)
     ''' tuples are immutable. I could remake the touple with list comprehension (which would
-    be hella cooler), however, this seems to be a better solution. I can just use '.remove'
+    be hecka cooler), however, this seems to be a better solution. I can just use '.remove'
     which I hope has better performance than forcing an iteration. '''
     print(words)
-    if "penis" in words:
-        print("Removing penis.")
-        words.remove("penis")
+    if "pen" in words:
+        print("Removing pen.")
+        words.remove("pen")
     print(words)
     
 def test_four(first, *words):
@@ -37,8 +37,8 @@ def test_five(list_one, list_two):
 
 def test():
     'test_one("Caterwal", "Cantonese")'
-    'test_two("cats", "va-gina", "penis")'
-    'test_three("pants", "penis")'
+    'test_two("cats", "va-gina", "pen")'
+    'test_three("pants", "pen")'
     'test_four("lower", "case", "words")'
     print(test_five(["apple", "banana", "orange"], ["apple", "banana", "orange"]))
     print(test_five(["apple", "banana"], ["apple", "banana", "orange"]))
